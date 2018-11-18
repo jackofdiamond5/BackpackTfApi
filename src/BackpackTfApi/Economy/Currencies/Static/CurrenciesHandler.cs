190aa5957d2309de449bdcabee0054cf3c549b84
jackofdiamond5 <jackofdiamond596@gmail.com>
2018-11-18 12:57:00 +0200

    feat: Added a CurrenciesHandler class

---
+++ src/BackpackTfApi/Economy/Currencies/Static/CurrenciesHandler.cs
@@ -0,0 +1,20 @@
+﻿using BackpackTfApi.Economy.Currencies.Models;
+using System;
+
+using BackpackTfApi.Static;
+
+namespace BackpackTfApi.Economy.Currencies.Static
+{
+    public class CurrenciesHandler
+    {
+        public static CurrencyType GetCurrencyData(string name, Response response)
+        {
+            if (response.Currencies.ContainsKey(name))
+            {
+                return response.Currencies[name];                
+            }
+
+            throw new InvalidOperationException(Messages.CurrencyNotFoundError);
+        }
+    }
+}
