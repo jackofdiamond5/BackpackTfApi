568def43adc73cc0e49f1c0b266c5537d7c3aa77
jackofdiamond5 <jackofdiamond596@gmail.com>
2018-11-21 19:55:35 +0200

    feat: Added models for GetUsers endpoint

---
+++ src/BackpackTfApi/WebApiUsers/WebUsers/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace BackpackTfApi.WebApiUsers.WebUsers.Models
+{
+    public class Response
+    {
+        [JsonProperty("success")]
+        public string Success { get; internal set; }
+
+        [JsonProperty("current_time")]
+        public long CurrentTime { get; internal set; }
+
+        [JsonProperty("players")]
+        public IReadOnlyDictionary<string, WebUser> Players { get; internal set; }
+    }
+}
---
+++ src/BackpackTfApi/WebApiUsers/WebUsers/Models/WebUser.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackpackTfApi.WebApiUsers.WebUsers.Models
+{
+    public class WebUser
+    {
+        [JsonProperty("steamid")]
+        public ulong SteamId { get; internal set; }
+
+        [JsonProperty("success")]
+        public int Success { get; internal set; }
+
+        [JsonProperty("backpack_value")]
+        public IReadOnlyDictionary<string, decimal> BackpackValue { get; internal set; }
+
+        [JsonProperty("backpack_update")]
+        public IReadOnlyDictionary<string, long> BackpackUpdate { get; internal set; }
+
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+
+        [JsonProperty("backpack_tf_trust")]
+        public IReadOnlyDictionary<string, int> BackpackTfTrust { get; internal set; }
+    }
+}
