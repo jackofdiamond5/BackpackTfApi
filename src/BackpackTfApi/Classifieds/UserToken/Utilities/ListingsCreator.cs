2e50956a2c21bfa8c0d9c5af14d668acde57dde1
jackofdiamond5 <jackofdiamond596@gmail.com>
2018-12-06 23:06:16 +0200

    feat: Including a listings creator utility class

---
+++ src/BackpackTfApi/Classifieds/UserToken/Utilities/ListingsCreator.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+using BackpackTfApi.Classifieds.UserToken.ListingsCreator.Models;
+
+namespace BackpackTfApi.Classifieds.UserToken.Utilities
+{
+    public static class ListingsCreator
+    {
+        public static Output CreateListing(Input input, string uri)
+        {
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    var response = client.UploadString(uri, JsonConvert.SerializeObject(input));
+                    return OutputData.FromJson(response);
+                }
+                catch (WebException ex)
+                {
+                    // TODO
+                    return null;
+                }
+            }
+        }
+    }
+}
