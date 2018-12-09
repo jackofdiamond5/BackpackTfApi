5157c76c43a3f83f4b7402a9b689b9bc0efc1496
jackofdiamond5 <jackofdiamond596@gmail.com>
2018-12-09 17:13:17 +0200

    feat: Added models for the users data endpoint

---
+++ src/BackpackTfApi/Classifieds/UserToken/Users/Models/Slots.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace BackpackTfApi.Classifieds.UserToken.Users.Models
+{
+    public class Slots
+    {
+        [JsonProperty("used")]
+        public int Used { get; internal set; }
+
+        [JsonProperty("total")]
+        public int Total { get; internal set; }
+    }
+}
