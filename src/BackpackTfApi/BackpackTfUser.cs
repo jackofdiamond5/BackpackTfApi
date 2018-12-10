b9f242ee33c604ad26e30aecf0f4237064291e0e
jackofdiamond5 <jackofdiamond596@gmail.com>
2018-12-10 18:24:58 +0200

    feat: Added the main user model and a static class for all API URIs

---
+++ src/BackpackTfApi/BackpackTfUser.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BackpackTfApi.Economy.SpecialItems;
+using BackpackTfApi.Economy.Prices.Models;
+using BackpackTfApi.Economy.Currencies.Models;
+using BackpackTfApi.Economy.PriceHistory.Models;
+using BackpackTfApi.WebApiUsers.WebUsers.Models;
+using BackpackTfApi.SteamUser.UserInventory.Models;
+using BackpackTfApi.WebApiUsers.WebImpersonatedUsers.Models;
+using BackpackTfApi.Classifieds.UserToken.UserListings.Models;
+using BackpackTfApi.Classifieds.UserToken.ListingsCreator.Models;
+using BackpackTfApi.Classifieds.UserToken.ClassifiedsSearch.Models;
+
+namespace BackpackTfApi
+{
+    public class BackpackTfUser
+    {
+        public BackpackTfUser(string steamid64, string apiKey, string accessToken)
+        {
+            this.SteamId64 = steamid64;
+            this.ApiKey = apiKey;
+            this.AccessToken = accessToken;
+        }
+
+        /// <summary>
+        /// The steamid64 associated with a steam account
+        /// </summary>
+        public string SteamId64 { get; set; }
+
+        /// <summary>
+        /// The API key provided by backpack.tf
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        /// <summary>
+        /// The access token provided by backpack.tf
+        /// </summary>
+        public string AccessToken { get; set; }
+
+        public CurrenciesData GetCurrencies(int raw)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PriceHistoryData GetPriceHistory(string baseItemName, string itemQuality)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PricesData GetPrices(int? raw, long? lastUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SpecialItemsData GetSpecialItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public WebUsersData GetUsersByIds(ICollection<string> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ImpersonatedUsersData GetImpersonatedUsers(int? limit, int? skip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClassifiedsData GetClassifieds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserListingsData GetOwnClassifieds(int? intent, int? inactive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OutputData CreateBuyListings(Input inputData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OutputData CreateSellListings(Input inputData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserInventoryData GetUserInventory(string steamid64)
+        {
+            var userInventoryUri =
+            $"http://steamcommunity.com/inventory/{this.SteamId64}/{BaseUris.AppId}/2?l=english&count=5000";
+
+            throw new NotImplementedException();
+        }
+
+        public UserInventoryData GetOwnInventory()
+        {
+            var userInventoryUri =
+            $"http://steamcommunity.com/inventory/{this.SteamId64}/{BaseUris.AppId}/2?l=english&count=5000";
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/BackpackTfApi/BaseUris.cs
@@ -0,0 +1,16 @@
+﻿namespace BackpackTfApi
+{
+    public static class BaseUris
+    {
+        public const string AppId = "440";
+        public const string GetCurrencies = "https://backpack.tf/api/IGetCurrencies/v1?key=";
+        public const string GetPriceHistory = "https://backpack.tf/api/IGetPriceHistory/v1?key=";
+        public const string GetPrices = "https://backpack.tf/api/IGetPrices/v4key=";
+        public const string GetSpecialItems = "https://backpack.tf/api/IGetSpecialItems/v1?key=";
+        public const string GetUsers = "https://backpack.tf/api/IGetUsers/v3?key=";
+        public const string GetImpersonatedUsers = "https://backpack.tf/api/IGetUsers/GetImpersonatedUsers?";
+        public const string ClassifiedsSearch = "https://backpack.tf/api/classifieds/search/v1?key=";
+        public const string UserListings = "https://backpack.tf/api/classifieds/listings/v1?token=";
+        public const string ClassifiedsCreate = "https://backpack.tf/api/classifieds/list/v1?token=";
+    }
+}
