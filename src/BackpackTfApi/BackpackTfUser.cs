7ed0c2026b754053e790a44beb40764b9d14e3dc
jackofdiamond5 <jackofdiamond596@gmail.com>
2018-12-10 18:55:10 +0200

    feat: Updated the main user model's functionality

--- src/BackpackTfApi/BackpackTfUser.cs
+++ src/BackpackTfApi/BackpackTfUser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
+
 
 using BackpackTfApi.Economy.SpecialItems;
 using BackpackTfApi.Economy.Prices.Models;
@@ -11,11 +13,16 @@ using BackpackTfApi.WebApiUsers.WebImpersonatedUsers.Models;
 using BackpackTfApi.Classifieds.UserToken.UserListings.Models;
 using BackpackTfApi.Classifieds.UserToken.ListingsCreator.Models;
 using BackpackTfApi.Classifieds.UserToken.ClassifiedsSearch.Models;
+using BackpackTfApi.Economy.Currencies.Static;
 
 namespace BackpackTfApi
 {
     public class BackpackTfUser
     {
+        private string steamId64 = "";
+        private string apiKey = "";
+        private string accessToken = "";
+
         public BackpackTfUser(string steamid64, string apiKey, string accessToken)
         {
             this.SteamId64 = steamid64;
@@ -26,21 +33,62 @@ namespace BackpackTfApi
         /// <summary>
         /// The steamid64 associated with a steam account
         /// </summary>
-        public string SteamId64 { get; set; }
+        public string SteamId64
+        {
+            get => this.steamId64;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException();
+
+                this.steamId64 = value;
+            }
+        }
 
         /// <summary>
         /// The API key provided by backpack.tf
         /// </summary>
-        public string ApiKey { get; set; }
+        public string ApiKey
+        {
+            get => this.apiKey;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException();
+
+                this.apiKey = value;
+            }
+        }
 
         /// <summary>
         /// The access token provided by backpack.tf
         /// </summary>
-        public string AccessToken { get; set; }
-
-        public CurrenciesData GetCurrencies(int raw)
+        public string AccessToken
         {
-            throw new NotImplementedException();
+            get => this.accessToken;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException();
+
+                this.accessToken = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns internal currency data for Team Fortress 2. 
+        /// </summary>
+        /// <param name="raw">
+        /// Set to 1 to include a ValueRaw property in the Price model(s). 
+        /// Set to 2 to include a ValueRawHigh property in the Price model(s).
+        /// </param>
+        /// <returns></returns>
+        /// <exception cref="WebException"></exception>
+        /// <exception cref="NotSupportedException"></exception>
+        public CurrenciesData GetCurrencies(int raw = 0)
+        {
+            var uri = new Uri(BaseUris.GetCurrencies + $"{this.ApiKey}&raw={raw}");
+            return CurrenciesHandler.DownloadCurrencyData(uri.ToString());
         }
 
         public PriceHistoryData GetPriceHistory(string baseItemName, string itemQuality)
@@ -91,7 +139,7 @@ namespace BackpackTfApi
         public UserInventoryData GetUserInventory(string steamid64)
         {
             var userInventoryUri =
-            $"http://steamcommunity.com/inventory/{this.SteamId64}/{BaseUris.AppId}/2?l=english&count=5000";
+            $"http://steamcommunity.com/inventory/{steamid64}/{BaseUris.AppId}/2?l=english&count=5000";
 
             throw new NotImplementedException();
         }
