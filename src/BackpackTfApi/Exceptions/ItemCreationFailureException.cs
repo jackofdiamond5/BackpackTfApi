877c2ea6ccb5e8ab61225824360bb2564b3519a2
jackofdiamond5 <jackofdiamond596@gmail.com>
2018-12-22 22:11:26 +0200

    feat: Add custom exception classes.

---
+++ src/BackpackTfApi/Exceptions/ItemCreationFailureException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BackpackTfApi.Exceptions
+{
+    public class ItemCreationFailureException : Exception
+    {
+        public ItemCreationFailureException(string message)
+            : base(message) { }
+    }
+}
---
+++ src/BackpackTfApi/Exceptions/ItemNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BackpackTfApi.Exceptions
+{
+    public class ItemNotFoundException : Exception
+    {
+        public ItemNotFoundException(string message)
+            : base(message) { }
+    }
+}
